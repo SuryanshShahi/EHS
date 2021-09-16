01248303ea34981c293dd2e8e3dfae7fdedf79e2
SuryanshShahi <suryansh06shahi@gmail.com>
2021-09-16 17:04:07 +0530

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,6 +15,16 @@ function Navbar() {
   };
   window.addEventListener("scroll", backtotop);
 
+  const Location = () => {
+    var x = document.getElementById("location");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  };
+
   return (
     <section>
       <div id="top">..</div>
@@ -48,18 +58,28 @@ function Navbar() {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-lg-auto mb-2 mb-lg-0 justify-content-center align-items-center d-flex">
               <div
+                onClick={Location}
+                className="fa fa-map-marker text-white fa-lg pr-3"
+                style={{ cursor: "pointer" }}
+              >
+                &nbsp;&nbsp;
+                <span className="font-weight-bold" style={{ fontSize: "17px" }}>
+                  Chandigarh
+                </span>{" "}
+                <b style={{ fontSize: "20px" }}>🡓</b>
+              </div>
+              <div
                 className="d-flex mr-lg-auto pt-lg-0 pt-4 align-items-center"
-                style={{ width: "100%", maxWidth: "400px" }}
+                style={{ width: "100%", maxWidth: "300px" }}
               >
                 <div
-                  className="fa fa-map-marker fa-2x position-absolute"
-                  style={{ padding: "3px 10px" }}
+                  className="fa fa-search fa-lg position-absolute"
+                  style={{ padding: "0px 10px" }}
                 ></div>
-
                 <input
                   className="form-control border-0"
                   type="search"
-                  placeholder="Search Location"
+                  placeholder="Serarch Service"
                   aria-label="Search"
                   style={{
                     borderRadius: "50px",
@@ -392,6 +412,85 @@ function Navbar() {
         >
           {" "}
         </a>
+      </div>
+
+      <div
+        id="location"
+        className="w-25 position-absolute border-0"
+        style={{
+          marginTop: "48px",
+          zIndex: "999",
+          height: "300px",
+          display: "none",
+          background: "rgb(222 224 236)",
+          left: "36%",
+          borderRadius: "10px",
+        }}
+      >
+        <div className="row px-4 py-2">
+          <div className="col">
+            <div className="fa fa-map-marker fa-lg pr-2"></div>Current Location
+          </div>
+          <div
+            className="col justify-content-end d-flex font-weight-bold text-primary"
+            style={{ fontSize: "14px" }}
+          >
+            Detact Using GPS
+          </div>
+        </div>
+        <hr style={{ height: "2px", margin: "0" }}></hr>
+
+        <div className="mx-4 mt-4 px-2">
+          <div
+            className="d-flex mr-lg-auto pt-lg-0 pt-4 align-items-center"
+            style={{ width: "100%" }}
+          >
+            <div
+              className="fa fa-search fa-sm pl-4 position-absolute"
+              style={{ padding: "0px 10px" }}
+            ></div>
+            <input
+              className="form-control border-0 py-2 pl-5"
+              type="search"
+              placeholder="Search for Locality/Appartment/Society"
+              aria-label="Search"
+              style={{
+                borderRadius: "18px",
+                width: "400px",
+                fontSize: "14px",
+                height: "41px",
+                maxHeight: "41px",
+              }}
+            />
+          </div>
+
+          <div
+            className="position-absolute pb-3"
+            style={{ maxHeight: "185px", bottom: "0" }}
+          >
+            <div
+              className="text-primary font-weight-bold py-1"
+            >
+              Saved Address
+            </div>
+            <div
+              className="fa fa-map-marker fa-sm"
+              // style={{ padding: "0px 10px" }}
+            >
+              <span className="pl-2 font-weight-bold" style={{ fontSize: "14px" }}>
+                Flat no 298, 1st Floor, TDI City, Sector 110, Mohali......
+              </span>
+            </div>
+            <div
+              className="fa fa-map-marker fa-sm"
+              // style={{ padding: "0px 10px" }}
+            >
+              <span className="pl-2 font-weight-bold" style={{ fontSize: "14px" }}>
+                Flat no 298, 1st Floor, TDI City, Sector 110, Mohali......
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
